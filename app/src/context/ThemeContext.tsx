9f457eb2192ed53d579238114a6c3a8f7d5c89af
Keaun Moughari <mougharik@gmail.com>
2020-05-16 19:37:42 -0400

    Add ThemeContext and empty TabNavigator file

---
+++ app/src/context/ThemeContext.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export const themes = {
+    Dark: {
+        dark: true,
+        colors: {
+        primary: '#F7A399',
+        background: '#323234',
+        card: 'rgb(255, 255, 255)',
+        text: '#FBF5F3',
+        border: '#FBF5F3',
+        },
+    }
+  };
---
+++ app/src/navigation/TabNavigator.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { RiArrowUpDownLine } from 'react-icons/ri';
+import { IoIosSettings } from 'react-icons/io';
+import { AiOutlineAreaChart } from 'react-icons/ai';
+
+const Tabs = createBottomTabNavigator();
+
+export default Tabs;
